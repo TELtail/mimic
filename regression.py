8f64cb2394e570a1232feb569723c2ea6ee9136c
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-05-31 10:49:06 +0900

    モデル作成スタート

--- regression.py
+++ regression.py
@@ -1,7 +1,18 @@
 import torch
 import torch.nn as nn
 
+class Net(nn.module):
+    def __init__(self):
+        super(Net,self).__init__()
 
+        
+    def forward(self,x):
+        
+        return x
+
+
+def mk_dataset():
+    pass
 
 def main():
     pass
