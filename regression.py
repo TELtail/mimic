aa1d24539cea5a21c5d96553f467903e55304ed4
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-05-31 16:32:29 +0900

    HR,RESP,SpO2,NBPSys,NBPDias,NBPMeanに選定

--- regression.py
+++ regression.py
@@ -32,21 +32,23 @@ def mk_data_pickle(dataset_path):
 
 
 def mk_dataset(data_pickle_path,age_pickle_path):
+    need_elements_list = ['HR', 'RESP', 'SpO2', 'NBPSys', 'NBPDias', 'NBPMean']
+
     merged_data = {}
 
     with open(data_pickle_path,"rb") as f:
         data = pickle.load(f)
     with open(age_pickle_path,"rb") as g:
         age_map = pickle.load(g)
-    
+    i=0
     for key,value in data.items():
         merged_data[key] = {}
         merged_data[key]["age"] = age_map[key]
         merged_data[key]["signals"] = value[0]
         merged_data[key]["fields"] = value[1]
-    
-
-    
+        if len(list(set(merged_data[key]["fields"]["sig_name"]) & set(need_elements_list))) == 6:
+            i += 1
+            print(i,merged_data[key]["fields"]["sig_name"])
 
 
 
