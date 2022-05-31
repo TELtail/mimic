71d49edffb83a2fe631f5bb52f2241c8a748752f
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-05-31 18:24:56 +0900

    不要な部分を削除したデータを取得できるように変更

--- regression.py
+++ regression.py
@@ -30,25 +30,43 @@ def mk_data_pickle(dataset_path):
     with open("data.bin","wb") as f:
         pickle.dump(data,f)
 
-
-def mk_dataset(data_pickle_path,age_pickle_path):
-    need_elements_list = ['HR', 'RESP', 'SpO2', 'NBPSys', 'NBPDias', 'NBPMean']
-
+def merging_data(data,age_map,need_elements_list):
     merged_data = {}
-
-    with open(data_pickle_path,"rb") as f:
-        data = pickle.load(f)
-    with open(age_pickle_path,"rb") as g:
-        age_map = pickle.load(g)
-    i=0
     for key,value in data.items():
         merged_data[key] = {}
         merged_data[key]["age"] = age_map[key]
         merged_data[key]["signals"] = value[0]
         merged_data[key]["fields"] = value[1]
-        if len(list(set(merged_data[key]["fields"]["sig_name"]) & set(need_elements_list))) == 6:
-            i += 1
-            print(i,merged_data[key]["fields"]["sig_name"])
+        if len(list(set(merged_data[key]["fields"]["sig_name"]) & set(need_elements_list))) != len(need_elements_list): #必要な要素を持っていなかったらデータ削除
+            del merged_data[key]
+    
+    return merged_data
+
+def extractioning_signals(merged_data,need_elements_list):
+    data_signlas_age = {}
+
+    for key,one_data in merged_data.items():
+        indexs = []
+        for element in need_elements_list:
+            indexs.append(one_data["fields"]["sig_name"].index(element)) #必要なindexを取得
+        data_signlas_age[key] = {}
+        data_signlas_age[key]["signals"] = one_data["signals"][:,indexs] #必要な信号を抽出
+        data_signlas_age[key]["age"] = merged_data[key]["age"]
+    
+    return data_signlas_age
+
+
+def mk_dataset(data_pickle_path,age_pickle_path):
+    need_elements_list = ['HR', 'RESP', 'SpO2', 'NBPSys', 'NBPDias', 'NBPMean']
+
+    with open(data_pickle_path,"rb") as f:
+        data = pickle.load(f) #信号データ
+    with open(age_pickle_path,"rb") as g:
+        age_map = pickle.load(g) #年齢の対応データ
+    
+    merged_data = merging_data(data,age_map,need_elements_list)
+    data_signals_age = extractioning_signals(merged_data,need_elements_list)
+    print(data_signals_age)
 
 
 
