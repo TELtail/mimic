8e4c71304d49eae6931b98b13ca1fe9958eea883
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-06-06 19:11:53 +0900

    demo.pyの機能を関数化

--- demo.py
+++ demo.py
@@ -2,17 +2,22 @@ from IPython.display import display
 import wfdb
 import numpy as np
 
+def plot_glaph(path,label_names):
+    signals,fields = wfdb.rdsamp(path)
+
+    label_index = []
+    print("-------------")
+    print(path)
+    for name in label_names:
+        idx = fields["sig_name"].index(name)
+        label_index.append(idx)
+        print(name,signals[:,idx].shape)
+    record = wfdb.rdrecord(path,channels=label_index)
+    print("-------------")
+    wfdb.plot_wfdb(record=record,title="Record")
+
+
 path = "./data\mimic-II/3000105n"
 label_names = ["HR","PULSE","SpO2"]
 
-
-signals,fields = wfdb.rdsamp(path)
-print(fields)
-display(signals)
-
-
-label_index = []
-for name in label_names:
-    label_index.append(fields["sig_name"].index(name))
-record = wfdb.rdrecord(path,channels=label_index)
-wfdb.plot_wfdb(record=record,title="Record")
+plot_glaph(path,label_names)
