9928b2f7ea79d85ea72e298dcb5e374ec61fc8af
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-06-06 18:52:40 +0900

    demo.pyを使いやすいように調整

--- demo.py
+++ demo.py
@@ -1,5 +1,18 @@
-f = open("./wfdb-python/sample-data/s0010_re.dat","rb")
+from IPython.display import display
+import wfdb
+import numpy as np
 
-data = f.read()
-print(type(data))
-print(len(data))
+path = "./data\mimic-II/3000105n"
+label_names = ["HR","PULSE","SpO2"]
+
+
+signals,fields = wfdb.rdsamp(path)
+print(fields)
+display(signals)
+
+
+label_index = []
+for name in label_names:
+    label_index.append(fields["sig_name"].index(name))
+record = wfdb.rdrecord(path,channels=label_index)
+wfdb.plot_wfdb(record=record,title="Record")
