7751aa41a10ab6108e5fb8de216033229c1232cc
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-06-07 18:52:53 +0900

    削除したデータの集計を行う機能の追加

--- regression.py
+++ regression.py
@@ -13,6 +13,7 @@ import datetime
 import shutil
 import argparse
 from logging import getLogger,config
+import logging
 import json
 SEED = 42
 
@@ -52,32 +53,38 @@ def mk_data_pickle(dataset_path):
         pickle.dump(data,f)
 
 def merging_data(data,age_map,need_elements_list):
-    merged_data = {}
+    data_not_have_feature = {} #削除したデータをカウントするための辞書
+    data_not_have_feature["not_have_need_feature"] = [] #必要な要素（特徴）を持っていなかった要素用
+    merged_data = {} #信号と年齢データをくっつけた辞書
     for key,value in data.items():
         merged_data[key] = {}
         merged_data[key]["age"] = age_map["data"][key]["age"]
         merged_data[key]["signals"] = value[0]
         merged_data[key]["fields"] = value[1]
         if len(list(set(merged_data[key]["fields"]["sig_name"]) & set(need_elements_list))) != len(need_elements_list): #必要な要素を持っていなかったらデータ削除
-            del merged_data[key]
+            data_not_have_feature["not_have_need_feature"].append([key,age_map["data"][key]["patient_number"]]) #使用しなかったデータの信号番号と患者番号を取得
+            del merged_data[key] #必要な要素（特徴）を持っていなかった要素を削除
     
-    return merged_data
+    return merged_data,data_not_have_feature
 
-def extractioning_signals(merged_data,need_elements_list):
-    data_signlas_age = {}
-
+def extractioning_signals(merged_data,age_map,need_elements_list,data_not_have_feature,minimum_signal_length):
+    data_signlas_age = {} #信号と年齢データをくっつけた辞書
+    data_not_have_feature["too_short"] = [] #データが短すぎた用
     for key,one_data in merged_data.items():
-        indexs = []
+        indexs = [] #必要な要素のあるインデックスを格納するリスト
+        if np.array(one_data["signals"]).shape[0] < minimum_signal_length: #短すぎるデータは削除
+            data_not_have_feature["too_short"].append([key,age_map["data"][key]["patient_number"]])
+            continue
         for element in need_elements_list:
             indexs.append(one_data["fields"]["sig_name"].index(element)) #必要なindexを取得
         data_signlas_age[key] = {}
         data_signlas_age[key]["signals"] = one_data["signals"][:,indexs] #必要な信号を抽出
         data_signlas_age[key]["age"] = merged_data[key]["age"]
-    
-    return data_signlas_age
+
+    return data_signlas_age,data_not_have_feature
 
 
-def mk_dataset(data_pickle_path,age_json_path,train_rate,batch_size,need_elements_list,minimum_signal_length,maximum_signal_length):
+def mk_dataset(data_pickle_path,age_json_path,train_rate,batch_size,need_elements_list,minimum_signal_length,maximum_signal_length,out_path):
 
 
     with open(data_pickle_path,"rb") as f:
@@ -85,15 +92,19 @@ def mk_dataset(data_pickle_path,age_json_path,train_rate,batch_size,need_element
     with open(age_json_path,"r") as g:
         age_map = json.load(g) #年齢の対応データ
     
-    merged_data = merging_data(data,age_map,need_elements_list) #信号と年齢データを対応付ける
-    data_signals_age = extractioning_signals(merged_data,need_elements_list) #必要なデータだけ取得
+    merged_data,data_not_have_feature = merging_data(data,age_map,need_elements_list) #信号と年齢データを対応付ける
+    data_signals_age,data_not_have_feature = extractioning_signals(merged_data,age_map,need_elements_list,data_not_have_feature,minimum_signal_length) #必要なデータだけ取得
+
+    
+    
+
+    delete_data_info(out_path,data_not_have_feature,age_map,len(data),len(data_signals_age)) #使用しなかったデータを集計してjsonファイルに出力する
 
     data_x = [] #信号
     data_t = [] #年齢(ラベル)
 
     for key,one_data in data_signals_age.items():
-        if np.array(one_data["signals"]).shape[0] < minimum_signal_length: #短すぎるデータは削除
-            continue
+        
         tmp = np.array(one_data["signals"],dtype=np.float32)[:maximum_signal_length]
         tmp = np.nan_to_num(tmp,nan=0) #nanを0で置換
         tmp = torch.tensor(tmp) 
@@ -112,6 +123,27 @@ def mk_dataset(data_pickle_path,age_json_path,train_rate,batch_size,need_element
     testloader  = torch.utils.data.DataLoader(testdataset,batch_size=1)
 
     return trainloader,testloader
+
+
+def delete_data_info(out_path,data_not_have_feature,age_map,before_num,after_num):
+    delete_data_json_path = os.path.join(out_path,"delete_data_info.json")
+
+    delete_data = {} 
+    delete_data["total"] = {"before":before_num,"after":after_num}
+    delete_data["correction"] = age_map["info"]
+    delete_data["delete"] = {}
+
+    for key,value in data_not_have_feature.items():
+        value = np.array(value)
+        tmp = value[:,1] #患者番号だけを抜き出し
+        tmp = set(tmp) #重複要素削除
+
+        delete_data["delete"][key] = {"signal_number":len(value),"patient_number":len(tmp)}
+
+    with open(delete_data_json_path,"w") as f:
+        json.dump(delete_data,f,indent=4)
+
+
 
 def train_method(trainloader,net,optimizer,loss_fn,device,batch_size):
     running_loss = 0
@@ -146,6 +178,8 @@ def test_method(testloader,net,optimizer,loss_fn,device):
 
     return running_loss
 
+
+
 def mk_out_dir(out_path):
     now = datetime.datetime.now() #現在時刻取得
     now_string = now.strftime("%Y-%m-%d-%H-%M-%S")
@@ -167,7 +201,6 @@ def plot_loss_glaph(epoch_loss,out_path):
     plt.savefig(png_path)
 
 def log_start(out_path,config_path):
-    log_path = os.path.join(out_path,"log.txt")
     with open(config_path,"r") as f:
         log_conf = json.load(f)
     log_conf["handlers"]["fileHandler"]["filename"] = os.path.join(out_path,"train_log.txt") #出力ログのpathを指定
@@ -237,7 +270,7 @@ def main():
 
     
     define_seed() #seed固定
-    trainloader,testloader = mk_dataset(data_pickle_path,age_json_path,train_rate,batch_size,need_elements_list,minimum_signal_length,maximum_signal_length) #データローダー取得
+    trainloader,testloader = mk_dataset(data_pickle_path,age_json_path,train_rate,batch_size,need_elements_list,minimum_signal_length,maximum_signal_length,out_path) #データローダー取得
     num_axis = len(need_elements_list)
     net = Net(num_axis,hidden_dim).to(device)
     logger.info(net)
@@ -245,7 +278,7 @@ def main():
 
     optimizer = torch.optim.Adam(net.parameters(),lr=lr)
     loss_fn = nn.MSELoss()
-    epoch_loss = []
+    epoch_loss = [] #グラフに出力するための損失格納用リスト
     try:
         for epoch in range(epochs):
             logger.info(f"----- epoch:{epoch+1} ---------------------------")
@@ -254,10 +287,12 @@ def main():
             epoch_loss.append([train_running_loss,test_running_loss])
     except KeyboardInterrupt:
         pass
+
     if len(epoch_loss) != 0:
-        plot_loss_glaph(epoch_loss,out_path)
+        plot_loss_glaph(epoch_loss,out_path) #1エポックでもあれば損失グラフ生成
     else:
-        shutil.rmtree(out_path)
+        logging.shutdown()
+        shutil.rmtree(out_path) #1エポックもなければディレクトリごと削除
 
 
 if __name__ == "__main__":
