3e933aa224b1e51d1ccf7fb8f8dc988470999e63
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-06-27 18:29:36 +0900

    コメント追加

--- age_inference/main.py
+++ age_inference/main.py
@@ -16,9 +16,9 @@ from train_test_loop_method import test_regression_method,train_regression_metho
 
 def main_method():
     args = get_parser()
-    out_path = mk_out_dir(args.out_path)
+    out_path = mk_out_dir(args.out_path) #出力ディレクトリ作成
     global logger
-    set_log_settings(out_path,args.config_path)
+    set_log_settings(out_path,args.config_path) #loggerの設定をjsonファイルから取得
     logger = log_start()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -26,30 +26,30 @@ def main_method():
     
     
 
-    print_parser(args)
+    print_parser(args) #取得したコマンドライン引数を表示
 
     define_seed() #seed固定
     data_x,data_t = mk_dataset_v2(args.data_pickle_path,args.age_json_path,args.need_elements_list,args.minimum_signal_length,args.maximum_signal_length,out_path,args.model_type)
     trainloader,testloader = get_loader(data_x,data_t,args.train_rate,args.batch_size)
-    out_dim,loss_fn = determing_setting(args.model_type)
-    num_axis = len(args.need_elements_list)
-    net = select_model(args.model_name,num_axis,args.hidden_dim,args.num_layers,args.maximum_signal_length,args.model_type,out_dim).to(device)
-    logger.info(net)
+    out_dim,loss_fn = determing_setting(args.model_type) #モデルタイプに対応したモデルの出力次元と損失関数を決定 
+    num_axis = len(args.need_elements_list) #入力次元数を決定
+    net = select_model(args.model_name,num_axis,args.hidden_dim,args.num_layers,args.maximum_signal_length,args.model_type,out_dim).to(device) #指定されたモデルを呼び出し
+    logger.info(net) #モデル情報出力
 
     optimizer = torch.optim.Adam(net.parameters(),lr=args.lr)
     epoch_loss = [] #グラフに出力するための損失格納用リスト
-    correct_for_classification = []
+    correct_for_classification = [] #分類問題時の正答率を格納するためのリスト
     try:
         for epoch in range(args.epochs):
             logger.info(f"----- epoch:{epoch+1} ---------------------------")
-            if args.model_type == "regression":
-                train_running_loss = train_regression_method(trainloader,net,optimizer,loss_fn,device,args.batch_size)
-                test_running_loss,predicted_for_plot = test_regression_method(testloader,net,loss_fn,device,args.print_result_flag)
-            elif args.model_type == "classification":
-                train_running_loss,train_correct = train_classification_method(trainloader,net,optimizer,loss_fn,device,args.batch_size)
-                test_running_loss,test_correct = test_classification_method(testloader,net,loss_fn,device,args.print_result_flag)
-                correct_for_classification.append([train_correct,test_correct])
-            epoch_loss.append([train_running_loss,test_running_loss])
+            if args.model_type == "regression": #回帰
+                train_running_loss = train_regression_method(trainloader,net,optimizer,loss_fn,device,args.batch_size) #学習
+                test_running_loss,predicted_for_plot = test_regression_method(testloader,net,loss_fn,device,args.print_result_flag) #テスト
+            elif args.model_type == "classification": #分類
+                train_running_loss,train_correct = train_classification_method(trainloader,net,optimizer,loss_fn,device,args.batch_size) #学習
+                test_running_loss,test_correct = test_classification_method(testloader,net,loss_fn,device,args.print_result_flag) #テスト
+                correct_for_classification.append([train_correct,test_correct]) #分類問題の結果を格納
+            epoch_loss.append([train_running_loss,test_running_loss]) #損失を格納
     except KeyboardInterrupt:
         pass
 
@@ -59,7 +59,7 @@ def main_method():
             plot_regression_inference_result(predicted_for_plot[:,1],predicted_for_plot[:,0],out_path) #最後のテスト結果をプロット
         elif args.model_type == "classification":
             correct_for_classification = np.array(correct_for_classification)
-            plot_classification_correct_result(correct_for_classification[:,0],correct_for_classification[:,1],out_path)
+            plot_classification_correct_result(correct_for_classification[:,0],correct_for_classification[:,1],out_path) #分類時の正答率の変遷グラフを作成
     else:
         logging.shutdown()
         shutil.rmtree(out_path) #1エポックもなければディレクトリごと削除
