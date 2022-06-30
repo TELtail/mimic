6a172b2adb3b6e6b1db6883698b5a4bfe8fc92cb
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-06-30 14:48:29 +0900

    学習データとテストデータの数を表示するように調整

--- age_inference/main.py
+++ age_inference/main.py
@@ -32,6 +32,7 @@ def main_method():
     define_seed() #seed固定
     data_x,data_t = mk_dataset_v2(args.data_pickle_path,args.age_json_path,args.need_elements_list,args.minimum_signal_length,args.maximum_signal_length,out_path,model_type)
     trainloader,testloader = get_loader(data_x,data_t,args.train_rate,args.batch_size)
+    logger.info("train:{} test:{}".format(len(trainloader.dataset),len(testloader.dataset)))
     out_dim,loss_fn = determing_setting(model_type) #モデルタイプに対応したモデルの出力次元と損失関数を決定 
     loss_fn = loss_fn.to(device)
     num_axis = len(args.need_elements_list) #入力次元数を決定
